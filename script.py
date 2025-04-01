2155ca09b2c5b63cb46290515194804350ddef4c
yui <149021353+misterycof@users.noreply.github.com>
2025-04-01 18:51:56 -0300

    Add files via upload

---
+++ script.py
@@ -0,0 +1,9 @@
+user = input("Crie seu user:")
+senha = input("Crie sua senha:")
+
+conta = [user, senha]
+
+login = [input("Digite seu user:"), input("Digite sua senha:")]
+
+if login == conta: print("Login feito com sucesso!")
+else: print("não foi maladnro")
