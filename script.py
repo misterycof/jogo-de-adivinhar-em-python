acdb61fceb03ad328c4a035b42b20774460fa56b
yui <149021353+misterycof@users.noreply.github.com>
2025-04-02 13:01:17 -0300

    Delete script.py

--- script.py
+++
@@ -1,9 +0,0 @@
-user = input("Crie seu user:")
-senha = input("Crie sua senha:")
-
-conta = [user, senha]
-
-login = [input("Digite seu user:"), input("Digite sua senha:")]
-
-if login == conta: print("Login feito com sucesso!")
-else: print("não foi maladnro")
